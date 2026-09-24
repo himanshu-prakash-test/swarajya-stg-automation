237a587818fe6d133a4ba256fc625029de388e86
himanshuprakash-cyber <himanshu.prakash@mitwpu.edu.in>
2026-09-24 15:53:22 +0530

    feat(customer-mgmt): isolate branch to customer update module

--- swarajya-create/purchase-order/po_pages/__init__.py
+++
@@ -1,5 +0,0 @@
-from .po_page import POPage
-from .form_executor import FormExecutor
-from .login_page import LoginPage
-
-__all__ = ["POPage", "FormExecutor", "LoginPage"]
--- swarajya-update/purchase-orders/po_update_pages/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Page Objects for Purchase Order Update module."""
-
-from .po_update_page import POUpdatePage
-from .login_page import LoginPage
-
-__all__ = ["POUpdatePage", "LoginPage"]
